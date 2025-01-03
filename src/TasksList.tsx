b36d9562f0ac8e33d6d2ee31ecbb5090492f6e08
Samira <samira73talanchi@gmail.com>
2025-01-03 13:38:37 +0330

    add tasklist show items

--- src/TasksList.tsx
+++ src/TasksList.tsx
@@ -17,6 +17,30 @@ export default function TasksList({tasks, toggleTaskDone, deleteTask} : TasksLis
     }
 
     return (
-
+        <ul style={{listStyleType: "none"}}>
+            {tasks.map((task, index) => (
+                <div key={`${index}-${task}`}>
+                    <input
+                        type="checkbox"
+                        checked={task.isDone}
+                        onChange={() => handleCheckboxChange(task.id)}
+                    />
+                    <li style={{
+                            display: "inline",
+                            textDecoration: task.isDone ? "line-through" : "none",
+                            marginLeft: "8px",
+                        }}
+                    >
+                        {task.title}
+                    </li>
+                    <button
+                        style={{ marginLeft: "8px" }}
+                        onClick={() => handleDeleteButton(task.id)}
+                    >
+                        Delete
+                    </button>
+                </div>      
+            ))}
+        </ul>
     );
 }
